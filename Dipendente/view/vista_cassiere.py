50c30c54ab7aa1c57519a45de3f518de1088d972
manuma <manuma.marrone@gmail.com>
2023-12-20 15:09:06 +0100

    collegamento con gestione clienti

--- Dipendente/view/vista_cassiere.py
+++ Dipendente/view/vista_cassiere.py
@@ -2,9 +2,17 @@ from PyQt6 import uic
 from PyQt6.QtWidgets import *
 from PyQt6.QtCore import pyqtSignal
 
+from ListaClienti.view.vista_lista_clienti import VistaListaClienti
+
 class VistaCassiere(QWidget):
     closed = pyqtSignal()
 
     def __init__(self, parent=None):
         super(VistaCassiere, self).__init__(parent)
         uic.loadUi('Dipendente/view/cassiereMain.ui', self)
+
+        self.clientiButton.clicked.connect(self.goGestioneClienti)
+
+    def goGestioneClienti(self):
+        self.vista_gestione_clienti = VistaListaClienti()
+        self.vista_gestione_clienti.show()
