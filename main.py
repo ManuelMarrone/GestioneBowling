a3e174db6f2c479dd65101f5d4640ce3a095f6c6
aleandro1219 <aleandromartini02@gmail.com>
2023-12-23 10:27:28 +0100

    Aggiunta del file pickle

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     VistaLogin.show()
     sys.exit(app.exec())
 
-
+#gfgfgf
 # if __name__ == '__main__':
 #     app = QApplication(sys.argv)
 #     vistaclienti = VistaGestioneClienti()
