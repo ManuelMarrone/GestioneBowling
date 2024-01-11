f12bfd5e55503adfc9ceb073d593c8001fe0276f
manuma <manuma.marrone@gmail.com>
2024-01-11 17:16:03 +0100

    lista scarpe del magazziniere e statistiche dei clienti e dei dipendenti

--- Amministratore/view/vista_amministratore.py
+++ Amministratore/view/vista_amministratore.py
@@ -3,6 +3,8 @@ from PyQt6 import uic
 from PyQt6.QtWidgets import *
 
 from Amministratore.view.vista_gestione_dipendenti import VistaGestioneDipendenti
+from Statistiche.controller.controllore_statistiche import ControlloreStatistiche
+
 "from Statistiche.controller.controllore_statistiche import ControlloreStatistiche"
 
 class VistaAmministratore(QWidget):
@@ -13,20 +15,74 @@ class VistaAmministratore(QWidget):
 
         self.gestioneDipendentiButton.clicked.connect(self.goDipendenti)
         self.esciButton.clicked.connect(self.chiudiFinestra)
+        self.resetComboBox()
         self.statComboBox.activated.connect(self.statistiche)
 
-    """def statistiche(self):
+    def statistiche(self):
         selezione = self.statComboBox.currentText()
+        if selezione == "":
+            self.etichetta1.setText("")
+            self.testo1.setText("")
+            self.etichetta2.setText("")
+            self.testo2.setText("")
+            self.etichetta3.setText("")
+            self.testo3.setText("")
+            self.etichetta4.setText("")
+            self.testo4.setText("")
+            self.etichetta5.setText("")
+            self.testo5.setText("")
+            self.etichetta6.setText("")
+            self.testo6.setText("")
+            self.etichetta7.setText("")
+            self.testo7.setText("")
         if selezione == "Dipendenti":
-            ControlloreStatistiche.statisticheDipendenti()
-     """
+            etaMediaDipendenti,totDipendenti = ControlloreStatistiche().etaMediaDipendenti()
+            percCassieri,percMagazzinieri = ControlloreStatistiche().percRuoliDipendenti()
+            percUomo,percDonna,percAltro = ControlloreStatistiche().percSessoDipendenti()
+            self.etichetta1.setText("Totale dipendenti: ")
+            self.testo1.setText(str(totDipendenti))
+            self.etichetta2.setText("Età media: ")
+            self.testo2.setText(str(etaMediaDipendenti)+" anni")
+            self.etichetta3.setText("Percentuale di cassieri: ")
+            self.testo3.setText(str(percCassieri) + "%")
+            self.etichetta4.setText("Percentuale di magazzinieri: ")
+            self.testo4.setText(str(percMagazzinieri) + "%")
+            self.etichetta5.setText("Percentuale di donne")
+            self.testo5.setText(str(percDonna) + "%")
+            self.etichetta6.setText("Percentuale di uomini")
+            self.testo6.setText(str(percUomo) + "%")
+            self.etichetta7.setText("Percentuale di altro")
+            self.testo7.setText(str(percAltro) + "%")
+        if selezione == "Clienti":
+            percAbbonati,clientiTot = ControlloreStatistiche().percClientiAbbonati()
+            tagliaPiuFrequente, numeroOccorrenze = ControlloreStatistiche().statisticaTaglieScarpe()
+            percClientiUomo, percClientiDonna, percClientiAltro = ControlloreStatistiche().percSessoClienti()
+            self.etichetta1.setText("Totale clienti: ")
+            self.testo1.setText(str(clientiTot))
+            self.etichetta2.setText("Percentuale di abbonati: ")
+            self.testo2.setText(str(percAbbonati) + "%")
+            self.etichetta3.setText("Taglia di scarpe più richiesta: ")
+            self.testo3.setText(str(tagliaPiuFrequente))
+            self.etichetta4.setText("Richiesta: ")
+            self.testo4.setText(str(numeroOccorrenze) + " volte")
+            self.etichetta5.setText("Percentuale di donne")
+            self.testo5.setText(str(percClientiDonna) + "%")
+            self.etichetta6.setText("Percentuale di uomini")
+            self.testo6.setText(str(percClientiUomo) + "%")
+            self.etichetta7.setText("Percentuale di altro")
+            self.testo7.setText(str(percClientiAltro) + "%")
 
     def goDipendenti(self):
         VistaAmministratore.close(self)
         self.vista_gestione_dipendenti = VistaGestioneDipendenti()
         self.vista_gestione_dipendenti.closed.connect(self.show)
+        self.vista_gestione_dipendenti.closed.connect(self.resetComboBox)
         self.vista_gestione_dipendenti.show()
 
+    def resetComboBox(self):
+        self.statComboBox.setCurrentIndex(0)
+        self.statistiche()
     def chiudiFinestra(self):
         self.closed.emit()
         self.close()
+
--- Statistiche/controller/controllore_statistiche.py
+++ Statistiche/controller/controllore_statistiche.py
@@ -1,21 +1,127 @@
+import os
+import pickle
+from datetime import datetime
+from collections import Counter
+
+from PyQt6.QtWidgets import QMessageBox
+
+
 class ControlloreStatistiche():
     def __init__(self):
-        pass
+        self.dipendenti = []
+        if os.path.isfile('Dipendente/data/dipendenti.pickle'):
+            with open('Dipendente/data/dipendenti.pickle', 'rb') as f:
+                self.dipendenti = pickle.load(f)
+        if len(self.dipendenti) == 0:
+            self.messaggio(tipo=0, titolo="Statistiche dipendenti", mex="Errore nel calcolo delle statistiche!")
 
-    def statisticheDipendenti(self):
-        #statistiche su età media
-        #numero dipendenti totali
+        self.clienti = []
+        if os.path.isfile('Cliente/data/ListaCLienti.pickle'):
+            with open('Cliente/data/ListaCLienti.pickle', 'rb') as f:
+                self.clienti = pickle.load(f)
+        if len(self.clienti) == 0:
+            self.messaggio(tipo=0, titolo="Statistiche clienti", mex="Errore nel calcolo delle statistiche!")
+    def etaMediaDipendenti(self):
+        self.oggi = datetime.now()
+        etaTot = 0
+        if len(self.dipendenti) > 0:
+            for dipendente in self.dipendenti:
+                eta = self.oggi.year - dipendente.dataNascita.date().year() - (
+                            (self.oggi.month, self.oggi.day) < (
+                dipendente.dataNascita.date().month(), dipendente.dataNascita.date().day()))
+                etaTot += eta
+        etaMedia = etaTot / len(self.dipendenti)
+        return int(etaMedia),len(self.dipendenti)
+
+    def percRuoliDipendenti(self):
         #%di cassieri e % di magazzinieri
+        contCas = 0
+        contMag = 0
+        if len(self.dipendenti) > 0:
+           for dipendente in self.dipendenti:
+               if(dipendente.ruolo=="Cassiere"):
+                  contCas += 1
+               else:
+                  contMag += 1
+        percCassieri = (contCas/len(self.dipendenti))*100
+        percMagazzinieri = (contMag/len(self.dipendenti))*100
+        percMagazzinieri = round(percMagazzinieri, 2)
+        percCassieri = round(percCassieri, 2)
+        return percCassieri,percMagazzinieri
+
+    def percSessoDipendenti(self):
         #%maschi e femmine
+        contM = 0
+        contF = 0
+        contAltro = 0
+        if len(self.dipendenti) > 0:
+            for dipendente in self.dipendenti:
+                if (dipendente.sesso == "Uomo"):
+                    contM += 1
+                elif(dipendente.sesso == "Donna"):
+                    contF += 1
+                else:
+                    contAltro += 1
+        percUomo = (contM / len(self.dipendenti)) * 100
+        percDonna = (contF / len(self.dipendenti)) * 100
+        percAltro = (contAltro / len(self.dipendenti)) * 100
+        percUomo = round(percUomo, 2)
+        percDonna = round(percDonna, 2)
+        percAltro = round(percAltro, 2)
+        return percUomo,percDonna,percAltro
 
-    def statisticheClienti(self):
+    def percClientiAbbonati(self):
         #% di abbonati
-        #taglie di scarpe più richieste
-        #% di maschi e femmine
-        #% età media
-        #numero di clienti
+        contAbbonati = 0
+        for cliente in self.clienti:
+            if (cliente.abbonato == True):
+                contAbbonati += 1
+        percAbbonati = (contAbbonati / len(self.clienti)) * 100
+        percAbbonati = round(percAbbonati, 2)
+        return percAbbonati,len(self.clienti)
 
+    def statisticaTaglieScarpe(self):
+        #taglie di scarpe più richieste
+        taglie = []
+        for cliente in self.clienti:
+            taglie.append(cliente.tagliaScarpe)
+        conteggioTaglie = Counter(taglie)
+        tagliaPiuFrequente, numeroOccorrenze = conteggioTaglie.most_common(1)[0]
+        return tagliaPiuFrequente,numeroOccorrenze
+
+    def percSessoClienti(self):
+        # %maschi e femmine
+        contM = 0
+        contF = 0
+        contAltro = 0
+        if len(self.clienti) > 0:
+            for cliente in self.clienti:
+                if (cliente.sesso == "Uomo"):
+                    contM += 1
+                elif (cliente.sesso == "Donna"):
+                    contF += 1
+                else:
+                    contAltro += 1
+        percUomo = (contM / len(self.clienti)) * 100
+        percDonna = (contF / len(self.clienti)) * 100
+        percAltro = (contAltro / len(self.clienti)) * 100
+        percUomo = round(percUomo, 2)
+        percDonna = round(percDonna, 2)
+        percAltro = round(percAltro, 2)
+        return percUomo, percDonna, percAltro
     def statistichePartite(self):
+        pass
         #numero di partite medio fatte da gruppi di clienti
         #partite totali fatte
         #tempo medio di una partita
+
+    def messaggio(self, tipo, titolo, mex):
+        mexBox = QMessageBox()
+        mexBox.setWindowTitle(titolo)
+        if tipo == 0:
+            mexBox.setIcon(QMessageBox.Icon.Warning)
+        elif tipo == 1:
+            mexBox.setIcon(QMessageBox.Icon.Information)
+        mexBox.setStyleSheet("background-color: rgb(54, 54, 54); color: white;")
+        mexBox.setText(mex)
+        mexBox.exec()
